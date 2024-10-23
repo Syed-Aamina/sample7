98a0ebf41411f36cf9fc1fa96a35ea02a0e19bfc
Syed-Aamina <228a1a4226@risekrishnasaiprakasam.edu.in>
2024-10-23 11:02:43 +0530

    Update ml.java

--- mina/ml.java
+++ mina/ml.java
@@ -1,7 +1,7 @@
 // HelloWorld.java
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello");
 
         // Creating an instance of the Calculator class
         Calculator calculator = new Calculator();
